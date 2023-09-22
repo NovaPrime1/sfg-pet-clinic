95f8fb0685c73f0d9b7e880d36c2d23e2b9d291c
NovaPrime1 <corey.powe@gmail.com>
2023-09-22 12:00:28 -0400

    closes #7

---
+++ pet-clinic-web/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/* trick with RequestMapping. So anything goes to owners with render this view. Because it is at the class level */
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"","/", "/index", "/index.html"})
+    public String listOwners(){
+
+        return "owners/index";
+    }
+}
