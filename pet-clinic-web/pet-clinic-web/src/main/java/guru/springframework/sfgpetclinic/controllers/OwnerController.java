9ecaeadb5a34d12d81df469594fdbfa109112362
NovaPrime1 <corey.powe@gmail.com>
2023-09-25 15:35:11 -0400

    adding Owners list and updated the OwnerController : Closes #16

--- pet-clinic-web/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /* trick with RequestMapping. So anything goes to owners with render this view. Because it is at the class level */
@@ -8,8 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    // Constructor injection is the preferred way working with injected properties. Only one way to instanciate it.
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll()); // give us a set to iterate over.
         return "owners/index";
     }
 }
