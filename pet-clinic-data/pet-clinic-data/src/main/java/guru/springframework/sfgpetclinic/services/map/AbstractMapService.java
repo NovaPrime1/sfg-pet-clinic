d74a9de76c0159e0403e9bd8637975cab4937cf5
NovaPrime1 <corey.powe@gmail.com>
2023-09-25 18:13:07 -0400

    Refactored Map impl to abstract ID generation : Closes #23

--- pet-clinic-data/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,17 +1,15 @@
 package guru.springframework.sfgpetclinic.services.map;
 
+import guru.springframework.sfgpetclinic.model.BaseEntity;
 import guru.springframework.sfgpetclinic.model.Owner;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
+// Refactored the ID to a long by extending the class to handled because java has a strongly type language.
+public class AbstractMapService <T extends BaseEntity, ID extends Long> {
 
-public class AbstractMapService <T, ID> {
-
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -22,8 +20,15 @@ public class AbstractMapService <T, ID> {
     }
 
     // We don't know what is coming back so we need to pass in (ID id) and take in an object
-    T saveById(ID id, T object) {
-        map.put(id, object);
+    T save( T object) { // removed ID id, because never used.
+        if(object !=null){
+            if(object.getId() == null){
+                   object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
 
         return object;
     }
@@ -35,6 +40,21 @@ public class AbstractMapService <T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+
+    // This is to automatically generate an ID based on collections.
+    private Long getNextId(){
+
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+
+        return nextId;
+
     }
 
 }
