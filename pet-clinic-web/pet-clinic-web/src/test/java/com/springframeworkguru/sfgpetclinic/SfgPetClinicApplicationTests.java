45d7ae32182f0d1b093d7969c04d20a4afcad3d3
NovaPrime1 <corey.powe@gmail.com>
2023-10-23 23:34:33 -0400

    Upgraded to JUnit 5. Closes #4

--- pet-clinic-web/pet-clinic-web/src/test/java/com/springframeworkguru/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/pet-clinic-web/src/test/java/com/springframeworkguru/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,13 +1,17 @@
 package com.springframeworkguru.sfgpetclinic;
 
+import guru.springframework.sfgpetclinic.SfgPetClinicApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
-class SfgPetClinicApplicationTests {
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes= SfgPetClinicApplication.class)
+public class SfgPetClinicApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
