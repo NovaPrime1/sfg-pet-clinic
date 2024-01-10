1989500192f4e03f681bde4df8bc92b630682777
NovaPrime1 <corey.powe@gmail.com>
2024-01-09 21:47:09 -0500

    Implemented find owner functionality Closes #54

--- pet-clinic-web/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,12 +1,18 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /* trick with RequestMapping. So anything goes to owners with render this view. Because it is at the class level */
 @RequestMapping("/owners")
@@ -20,17 +26,51 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"","/", "/index", "/index.html"})
-    public String listOwners(Model model){
-
-        model.addAttribute("owners", ownerService.findAll()); // give us a set to iterate over.
-        return "owners/index";
+    // Spring to auto bind variables in the application.  ex.Http var bind to java objects
+    // This feature helps your control the magic. How we handle post but spring is moving to restful services
+    // Handling form post to controller. Setting a disallow for id - defensive coding.
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
     }
 
-    @RequestMapping("/find" )
-    public String findOwners(){
+//    @RequestMapping({"","/", "/index", "/index.html"})
+//    public String listOwners(Model model){
+//
+//        model.addAttribute("owners", ownerService.findAll()); // give us a set to iterate over.
+//        return "owners/index";
+//    }
 
-        return "notimplemented";
+    @RequestMapping("/find" )
+    public String findOwners(Model model){
+        model.addAttribute("owner", Owner.builder().build());
+
+        return "owners/findOwners";
+    }
+
+    @GetMapping
+    public String processFindForm(Owner owner, BindingResult result, Model model){
+        // allow parameterless GET request for /owners to return all records
+        if (owner.getLastName() == null) {
+            owner.setLastName(""); // empty string signifies broadest possible search
+        }
+
+        // find owners by last name
+        List<Owner> results = ownerService.findAllByLastNameLike("%"+ owner.getLastName() + "%");
+
+        if (results.isEmpty()) {
+            // no owners found
+            result.rejectValue("lastName", "notFound", "not found");
+            return "owners/findOwners";
+        } else if (results.size() == 1) {
+            // 1 owner found
+            owner = results.get(0);
+            return "redirect:/owners/" + owner.getId();
+        } else {
+            // multiple owners found
+            model.addAttribute("selections", results);
+            return "owners/ownersList";
+        }
     }
 
     @GetMapping("/{ownerId}")
--- pet-clinic-web/pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -53,42 +54,38 @@ class OwnerControllerTest {
 
     }
 
-    @Test
-    void listOwners() throws Exception {
-        when(ownerService.findAll()).thenReturn(owner);
-
-        // Testing list owners by hitting the url, check status the checking view name and expecting Model to have a size 2
-        mockMvc.perform(get( "/owners"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("owners/index"))
-                .andExpect((model().attribute("owners", hasSize(2))));
-
-
-    }
-
-
-    // Testing alternate patch to see if the path is correct.
-    @Test
-    void listOwnersByIndex() throws Exception {
-        when(ownerService.findAll()).thenReturn(owner);
-
-        mockMvc.perform(get( "/owners/index"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("owners/index"))
-                .andExpect((model().attribute("owners", hasSize(2))));
-
-
-    }
 
     @Test
     void findOwners() throws Exception {
         mockMvc.perform(get("/owners/find"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("notimplemented"));
+                .andExpect(view().name("owners/findOwners"))
+                .andExpect(model().attributeExists("owner"));
 
         // Make sure we have zero interaction with the Mock ownerService
-        verifyNoInteractions(ownerService);
+        verifyNoInteractions(ownerService); // verifyZeroInteraction(ownerService) - find this code
 
+    }
+
+
+    @Test
+    void processFindFormReturnMany() throws Exception {
+        when(ownerService.findAllByLastNameLike(anyString())).thenReturn(Arrays.asList(Owner.builder().id(1L).build(),
+                Owner.builder().id(2l).build()));;
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"))
+                .andExpect(model().attribute("selections", hasSize(2)));
+    }
+
+    @Test
+    void processFindFormReturnOne() throws Exception {
+        when(ownerService.findAllByLastNameLike(anyString())).thenReturn(Arrays.asList(Owner.builder().id(1L).build()));
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
 
 
     }
